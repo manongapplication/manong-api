c4f947eb53a91024e544fb49f50355167bca5ad5
CharlesCraft50 <charlescraft50yt@gmail.com>
2025-10-27 17:58:04 +0800

    dev

--- src/env.controller.ts
+++ src/env.controller.ts
@@ -4,6 +4,10 @@ import { Controller, ForbiddenException, Get, Req } from '@nestjs/common';
 export class EnvController {
   @Get()
   getEnv(@Req() req) {
+    if (process.env.NODE_ENV === 'development') {
+      return { API_URL: process.env.API_URL };
+    }
+
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     if (!req.headers.referer?.includes('manongapp.com')) {
       throw new ForbiddenException();
