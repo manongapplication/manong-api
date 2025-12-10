2c70d77c6eb912d8b0b2bea3bd5b6711f2de758e
CharlesCraft50 <charlescraft50yt@gmail.com>
2025-12-10 16:57:11 +0800

    Directions API

--- src/directions/directions.module.ts
+++ src/directions/directions.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { DirectionsController } from './directions.controller';
+import { AppMaintenanceGuard } from 'src/common/guards/app-maintenance.guard';
+import { AppMaintenanceService } from 'src/app-maintenance/app-maintenance.service';
+import { DirectionsService } from './directions.service';
 
 @Module({
-  controllers: [DirectionsController],
+  imports: [],
+  controllers: [
+    DirectionsController,
+    AppMaintenanceGuard,
+    AppMaintenanceService,
+  ],
+  providers: [DirectionsService],
+  exports: [DirectionsService],
 })
 export class DirectionsModule {}
