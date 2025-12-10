f64a17ccdae6b633884b9587d912cac98b5c066b
CharlesCraft50 <charlescraft50yt@gmail.com>
2025-12-10 18:11:43 +0800

    S

--- src/directions/directions.module.ts
+++ src/directions/directions.module.ts
@@ -3,15 +3,13 @@ import { DirectionsController } from './directions.controller';
 import { AppMaintenanceGuard } from 'src/common/guards/app-maintenance.guard';
 import { AppMaintenanceService } from 'src/app-maintenance/app-maintenance.service';
 import { DirectionsService } from './directions.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [
-    DirectionsController,
-    AppMaintenanceGuard,
-    AppMaintenanceService,
-  ],
-  providers: [DirectionsService],
+  imports: [PrismaModule, UserModule],
+  controllers: [DirectionsController],
+  providers: [DirectionsService, AppMaintenanceGuard, AppMaintenanceService],
   exports: [DirectionsService],
 })
 export class DirectionsModule {}
